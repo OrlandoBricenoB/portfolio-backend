c1f66a7db17dff7086e0843579e882072d87f4ef
orlandobricenob <orlando.briceno.blanco@gmail.com>
2022-12-28 14:59:34 -0400

    feat: Remove lastname, just use name

--- models/User.js
+++ models/User.js
@@ -3,7 +3,6 @@ const { Schema, model } = require('mongoose')
 const userSchema = new Schema({
   uuid: String,
   name: String,
-  lastname: String,
   image: String,
   email: String,
   createDate: Date
