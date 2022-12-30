07d0195d537fa6d39840d1faecb83430f0012e22
orlandobricenob <orlando.briceno.blanco@gmail.com>
2022-12-29 21:42:16 -0400

    feat: Sirv public folder

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express()
 // * Allow body with json data.
 app.use(express.json({ limit: '5mb' }))
 app.use(cors())
+app.use(express.static('public'))
 
 // * Import database connect.
 const connect = require('./connection')
